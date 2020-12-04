c53da616d1e42ab155456fe242e093abaf9dda07
raianap76 <raianap76@gmail.com>
2020-12-03 21:31:28 -0300

    deafio-dev

--- script/blog.js
+++ script/blog.js
@@ -1,4 +1,6 @@
-
+/**
+ * Inserir o conteudo armazenado no localStorage na div onde esta o id user
+ */
 window.onload = function() { 
     document.getElementById('user').textContent = `Olá ${localStorage.name}!`;
     
